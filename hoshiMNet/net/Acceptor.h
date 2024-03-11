64cdfaeb9e9351f3aedea1894b7da3c0d6ff1ad6
1224138161 <1224138161@qq.com>
2024-03-11 16:51:24 +0800

    feat(Acceptor class): add Acceptor
    build: link hoshiMNetNet with hoshiMNetBase

--- hoshiMNet/net/Acceptor.h
+++ hoshiMNet/net/Acceptor.h
@@ -1,15 +1,37 @@
 #ifndef HOSHIMNET_NET_ACCEPTOR_H_
 #define HOSHIMNET_NET_ACCEPTOR_H_
 
+#include "Channel.h"
+#include "Socket.h"
+
 namespace hoshiMNet
 {
 namespace net
 {
 
+class EventLoop;
+
 class Acceptor
 {
 public:
-    
+    using NewConnectionCallback = std::function<void(int sockfd, const InetAddress&)>;
+
+public:
+    Acceptor(EventLoop* loop, const InetAddress& listenAddr);
+    ~Acceptor();
+
+    void listen();
+
+    void setNewConnectionCallback(const NewConnectionCallback& cb) { newConnectionCallback_ = cb; }
+
+private:
+    void acceptNewConnection();
+
+private:
+    EventLoop* loop_;
+    Socket socket_;
+    Channel channel_;
+    NewConnectionCallback newConnectionCallback_;
 };
 
 } // namespace net
